eaf630078105b215c3f2406c528786b2ce0c163a
Spencer Greene <sgreene@freetoast.com>
2017-12-15 10:05:44 -0800

    create new note, upload attachment to s3 bucket

--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    BUCKET: "sg-serverless-stack-tutorial-01"
+  },
+  apiGateway: {
+    URL: "https://gix05r7yk3.execute-api.us-west-2.amazonaws.com/prod",
+    REGION: "us-west-2"
+  },
   cognito: {
     USER_POOL_ID: "us-west-2_My3D9CdYA",
-    APP_CLIENT_ID: "5kjlm8l27elvpjnmdg5dreqsg3"
+    APP_CLIENT_ID: "5kjlm8l27elvpjnmdg5dreqsg3",
+    REGION: "us-west-2",
+    IDENTITY_POOL_ID: "us-west-2:8b204151-ed59-4e17-b3cc-484ab5d03123"
   }
 }
